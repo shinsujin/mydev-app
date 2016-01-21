744f96093ce0c937134f90263f4e5a4cb871718f
gahee811 <gahee811@icloud.com>
2016-01-21 17:50:43 +0900

    git 테스트

---
+++ MyApplication/app/src/main/gen/com/example/ktds/myapplication/Manifest.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.ktds.myapplication;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+    //git test !!
+}
