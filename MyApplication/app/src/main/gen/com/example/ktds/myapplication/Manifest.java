7d49fb61407013659300b55fe02646ccd80cb4e9
gahee811 <gahee811@icloud.com>
2016-01-21 18:02:33 +0900

    dd!!

--- MyApplication/app/src/main/gen/com/example/ktds/myapplication/Manifest.java
+++ MyApplication/app/src/main/gen/com/example/ktds/myapplication/Manifest.java
@@ -4,5 +4,5 @@ package com.example.ktds.myapplication;
 
 /* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
 public final class Manifest {
-    //git test !!  [2016-01-21] by gahee
+    //git test !!  [2016-01-21] by gaheeddd
 }
